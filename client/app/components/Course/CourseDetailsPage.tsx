d8006d67c6ba728ef1243c6b5e7f70d97108d66e
Mateeb Haider <muteebhdr@gmail.com>
2026-01-10 16:45:03 +0500

    feat: course details page

--- client/app/components/Course/CourseDetailsPage.tsx
+++ client/app/components/Course/CourseDetailsPage.tsx
@@ -5,33 +5,38 @@ import Heading from "@/app/utils/Heading";
 import Header from "../Header";
 import Footer from "../Route/Footer";
 import CourseDetail from "./CourseDetail";
-type Props={
-id:string
-}
-const CourseDetailsPage = ({id}: Props) => {
-    const [route, setRoute] = useState('Login');
-    const [open, setOpen] = useState(false);
-    const {data, isLoading, isError}=useGetCourseDetailsQuery({id});
+type Props = {
+  id: string;
+};
+const CourseDetailsPage = ({ id }: Props) => {
+  const [route, setRoute] = useState("Login");
+  const [open, setOpen] = useState(false);
+  const { data, isLoading, isError } = useGetCourseDetailsQuery({ id });
 
+  return (
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div>
+          <Heading
+            title={data?.course?.name + " - Elearning"}
+            description="E learing is community where people learn with parctical implementation"
+            keywords={data?.course?.tags}
+          />
+          <Header
+            route={route}
+            setRoute={setRoute}
+            open={open}
+            setOpen={setOpen}
+            activeItem={1}
+          />
+          <CourseDetail data={data?.course} />
+          <Footer />
+        </div>
+      )}
+    </>
+  );
+};
 
-  return <>
-  {isLoading ? <Loader/>: 
-  (
-<div>
-    <Heading  title={data.course.name+ " - Elearning"} description="E learing is community where people learn with parctical implementation" keywords={data?.course?.tags}/>
-    <Header 
-    route={route}
-    setRoute={setRoute}
-    open={open}
-    setOpen={setOpen}
-    activeItem={1}
-    />
-    <CourseDetail data={data.course}/>
-    <Footer/>
-</div>  
-)
-  }
-  </>
-}
-
-export default CourseDetailsPage
+export default CourseDetailsPage;
