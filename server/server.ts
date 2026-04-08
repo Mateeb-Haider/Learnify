306bf2c68d8035456b5d432458189fae7188b7cc
Mateeb Haider <muteebhdr@gmail.com>
2026-04-08 07:50:41 +0500

    feat: basic socket server configration

--- server/server.ts
+++ server/server.ts
@@ -1,16 +1,24 @@
-import 'dotenv/config';
+// import 'dotenv/config';
 import { app } from "./app";
 import connectDB from "./utils/db";
 import { v2 as cloudinary } from 'cloudinary'
+import http from "http";
+import { initSocketServer } from './socketServer';
+require("dotenv").config();
+const server = http.createServer(app);
+
+
 // Cloudinary config
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.CLOUD_API_KEY,
     api_secret: process.env.CLOUD_SECRET_KEY,
 });
+
+initSocketServer(server);
 // Create Server
 const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is connected with port ${PORT}`);
     connectDB();
 });
